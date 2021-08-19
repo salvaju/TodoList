aa710a3346b472d04bc83c31d36b69bf8119a0df
Juan Carlos Salvador <salvaju@oocl.com>
2021-08-19 23:46:07 +0800

    [TodoList][Juan Carlos Salvador] feat: Added modal for updating text API

--- src/features/todos/components/TodoItem.js
+++ src/features/todos/components/TodoItem.js
@@ -1,44 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { selectTodoById, ToggleTodo, RemoveTodo} from '../reducers/todosSlice';
 import "../styles/TodoItem.css";
 import {updateTodo, deleteTodo} from '../../apis/todos';
 import {DeleteOutlined, FormOutlined} from '@ant-design/icons'
+import { Modal, Input } from 'antd';
 
 function TodoItem(props) {
 
-    //const todo = getTodoById(initialTodoList, props.itemId)
     const todo = useSelector(
         (state) => selectTodoById(state, props.itemId)
     )
 
     const dispatch = useDispatch();
-
-    const todoStatus = todo.done ? "done" : ""
+    const todoStatus = todo.done ? "done" : "";
+    const { TextArea } = Input;
 
     function handleClick() {
         updateTodo(props.itemId, {done: !todo.done}).then((response) => {
-            console.log("response: ", response);
             dispatch(ToggleTodo({id: props.itemId, updateTodo: response.data}));
         });
     }
 
-    function handleRemove(event) {
-        event.stopPropagation();
-
+    function handleRemove() {
         deleteTodo(todo.id).then((response) => {
             console.log("response: ", response);
             dispatch(RemoveTodo(todo.id));
         })
     }
 
-    return (
-        <div className = {`TodoItem-todo ${todoStatus}`} onClick = {handleClick}>
 
-        < FormOutlined className = "update" /> 
-        {todo.text} <DeleteOutlined className = "remove" onClick = {handleRemove}  />   
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [todoText, setTodoText] = useState("");
+    
+    const openModal = () => {
+        setIsModalVisible(true);
+    }
+ 
+    const handleOk = () => {
+        if (todoText === "") {
+
+            setIsModalVisible(false);
             
-        </div>
+        } else {
+
+            updateTodo(props.itemId, {text: todoText}).then((response) => {
+                console.log("todoText", todoText);
+                console.log("response", response.data);
+                dispatch(ToggleTodo({id: props.itemId, updateTodo: response.data}));
+            });
+
+            setIsModalVisible(false);
+            setTodoText("");
+
+        }
+      };
+    
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
+
+    function handleSetTodoText (event) {
+        setTodoText(event.target.value);
+    }
+
+    return (
+        <div className = {`TodoItem-todo ${todoStatus}`}>
+
+            <FormOutlined className = "update" onClick = {openModal}/>
+            <span onClick = {handleClick}> {todo.text} </span>
+            <DeleteOutlined className = "remove" onClick = {handleRemove}/>
+
+            <Modal title = 'Edit Todo' visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+                        <TextArea
+                            placeholder = {todo.text}
+                            value = {todoText}
+                            onChange = {handleSetTodoText}
+                        />
+            </Modal>
+
+        </div> 
     );
 }
 
